3d9b880037c5c1343c1d3ba592e4b33d59187c18
Terence Kruger <terencejkruger@gmail.com>
2021-01-14 17:19:58 +1030

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+const debug = require("debug");
+
+module.exports = (prefix) => {
+  return {
+    info: debug(`${prefix}:info`),
+    error: debug(`${prefix}:error`),
+    warning: debug(`${prefix}:warning`),
+    dev: debug(`${prefix}:dev`),
+  };
+};
