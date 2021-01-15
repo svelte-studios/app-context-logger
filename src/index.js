7aebf643ebe59c20eca851b2309c495825dd0805
Terence Kruger <terencejkruger@gmail.com>
2021-01-15 13:33:37 +1030

    Husky 5 and fixed logger

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const debug = require('debug');
 
 module.exports = prefix => {
   return {
-    info: debug(`${prefix}:info2`),
+    info: debug(`${prefix}:info`),
     error: debug(`${prefix}:error`),
     warning: debug(`${prefix}:warning`),
     dev: debug(`${prefix}:dev`),
