4be3a52807cfe5e7f6ccb42d2dd94a66a679cca8
Terence Kruger <terencejkruger@gmail.com>
2021-01-15 13:18:42 +1030

    testing husky

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const debug = require('debug');
 
 module.exports = prefix => {
   return {
-    info: debug(`${prefix}:info`),
+    info: debug(`${prefix}:info2`),
     error: debug(`${prefix}:error`),
     warning: debug(`${prefix}:warning`),
     dev: debug(`${prefix}:dev`),
